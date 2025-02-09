f274a08f336d34307b4863d5386f325144529de9
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-02-09 23:56:08 +0600

    implemnt grid in hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Oswald } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const oswald = Oswald({
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   title: "Alomgir Hasan Shakib Portfolio",
@@ -18,11 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${oswald.className}  antialiased`}
-      >
-        {children}
+    <html lang="en"  suppressHydrationWarning>
+      <body className={`${oswald.className}  antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
